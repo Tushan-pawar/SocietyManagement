de6868d4e0bd4f80c60df497983e7dc410ffd654
Tushan-pawar <106312612+Tushan-pawar@users.noreply.github.com>
2023-11-14 07:21:18 +0530

    Resolved conflicts AdminController.java

--- src/main/java/com/springboot/main/controller/AdminController.java
+++ src/main/java/com/springboot/main/controller/AdminController.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 package com.springboot.main.controller;
 
 import java.util.List;
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 1d7f627400cf2ed935338c192d15398243bbc147
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -66,77 +60,3 @@ public class AdminController {
 	}
 
 }
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
-=======
-package com.springboot.main.controller;
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.springboot.main.exception.InvalidIdException;
-import com.springboot.main.model.Admin;
-import com.springboot.main.model.User;
-import com.springboot.main.service.AdminService;
-import com.springboot.main.service.UserService;
-
-@RestController
-
-@RequestMapping("/admin")
-public class AdminController {
-	@Autowired
-	private AdminService adminService;
-
-	//@PostMapping("/admin/add")
-	@Autowired
-	private UserService userService;
-
-	/* add the admin */
-	@PostMapping("/add")
-	public Admin insertAdmin(@RequestBody Admin admin) {
-		User user = admin.getUser();
-		user.setRole("ADMIN");
-		user = userService.insert(user);
-		admin.setUser(user);
-		return adminService.insert(admin);
-	}
-	@GetMapping("/getall")
-	
-	public List<Admin> getAllAdmins(){
-	List<Admin> admin = adminService.getAllAdmins();
-	return admin;
-		
-	}
-	@GetMapping("/getone/{id}")
-	public ResponseEntity<?> getOne(@PathVariable("id") int id) {
-
-		try {
-			Admin admin = adminService.getOne(id);
-			return ResponseEntity.ok().body(admin);
-		} catch (InvalidIdException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-
-	}
-	
-}
->>>>>>> bc1f25922778682115cb590275a680864198a72b
-<<<<<<< HEAD
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
-=======
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
->>>>>>> 1d7f627400cf2ed935338c192d15398243bbc147
