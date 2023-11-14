8f7b3f178af92c3d8c96bd8a3408bc33bf7d6d82
Tushan-pawar <106312612+Tushan-pawar@users.noreply.github.com>
2023-11-14 07:28:25 +0530

    Update EventController.java

--- src/main/java/com/springboot/main/controller/EventController.java
+++ src/main/java/com/springboot/main/controller/EventController.java
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
-=======
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
->>>>>>> 1d7f627400cf2ed935338c192d15398243bbc147
 package com.springboot.main.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,14 +19,7 @@ public class EventController {
         return event;
     }
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
-=======
+
 package com.springboot.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,8 +45,6 @@ public Event insertEvent(@RequestBody Event event) {
 
 
 }
->>>>>>> bc1f25922778682115cb590275a680864198a72b
-<<<<<<< HEAD
 >>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
 =======
 >>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
