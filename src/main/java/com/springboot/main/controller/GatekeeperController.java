d449d8f35285fca5353a8af6c63a1875f5df9e84
Tushan-pawar <106312612+Tushan-pawar@users.noreply.github.com>
2023-11-14 07:29:21 +0530

    Update GatekeeperController.java

--- src/main/java/com/springboot/main/controller/GatekeeperController.java
+++ src/main/java/com/springboot/main/controller/GatekeeperController.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.springboot.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,71 +35,3 @@ public class GatekeeperController {
 	}
 
 }
-=======
-package com.springboot.main.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.springboot.main.model.GateKeeper;
-import com.springboot.main.model.User;
-import com.springboot.main.service.GateKeeperService;
-import com.springboot.main.service.UserService;
-
-@RestController
-@RequestMapping("/gatekeeper")
-public class GateKeeperController {
-
-	@Autowired
-	private GateKeeperService gatekeeperService;
-	@Autowired
-	private UserService userService;
-
-	@PostMapping("/add")
-	public GateKeeper insertGateKeeper(@RequestBody GateKeeper gatekeeper) {
-		User user = gatekeeper.getUser();
-		user.setRole("GATEKEEPER");
-		user= userService.insert(user);
-		gatekeeper.setUser(user);
-		return gatekeeperService.insert(gatekeeper);
-	}
-
-}
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
-=======
-package com.springboot.main.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.springboot.main.model.GateKeeper;
-import com.springboot.main.model.User;
-import com.springboot.main.service.GateKeeperService;
-import com.springboot.main.service.UserService;
-
-@RestController
-@RequestMapping("/gatekeeper")
-public class GateKeeperController {
-
-	@Autowired
-	private GateKeeperService gatekeeperService;
-	@Autowired
-	private UserService userService;
-
-	@PostMapping("/add")
-	public GateKeeper insertGateKeeper(@RequestBody GateKeeper gatekeeper) {
-		User user = gatekeeper.getUser();
-		user.setRole("GATEKEEPER");
-		user= userService.insert(user);
-		gatekeeper.setUser(user);
-		return gatekeeperService.insert(gatekeeper);
-	}
-
-}
->>>>>>> 5dc1a4fcc131349ec52e4e213d2b9f911445b8ce
